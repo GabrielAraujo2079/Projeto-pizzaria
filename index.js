5a58e5d579cba9d040d0553475b2540a06da745a
GabrielAraujo2079 <gabrielaraujo20079@gmail.com>
2025-09-08 10:56:45 -0300

    Troquei if/else por switch case

--- index.js
+++ index.js
@@ -3,57 +3,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var input = require("prompt-sync")();
 var arquivo = "usuarios.json";
-/*Cadastro dos Clientes */
+// Verifica se o arquivo JSON existe, se não cria um vazio
 if (!fs.existsSync(arquivo)) {
     fs.writeFileSync(arquivo, "[]");
 }
 // lê os usuários do arquivo JSON e coloca no array
 var usuarios = JSON.parse(fs.readFileSync(arquivo, "utf-8"));
 var _loop_1 = function () {
-    console.log("[1]Cadastro \n[2]logar [3]Sair");
-    var cliente = input("Digite oque deseja fazer: ");
-    if (cliente == "1") {
-        var nome = input("Digite Seu nome: ");
-        var senha = input("Crie uma senha: ");
-        var cpf_1 = input("Digite seu CPF: ");
-        var email = input("Digite seu email: ");
-        var dataNascmtoInput = input("Digite sua data de nascimento (AAAA-MM-DD): ");
-        var dataNascimento = new Date(dataNascmtoInput);
-        var hoje = new Date();
-        if (dataNascimento > hoje) {
-            console.log("Data de nascimento inválida! Não pode ser no futuro.");
-            return "continue";
-        }
-        if (usuarios.find(function (u) { return u.cpf == cpf_1; })) {
-            console.log("Usuario ja existe: ");
-            return "continue";
-        }
-        usuarios.push({ nome: nome, senha: senha, cpf: cpf_1, email: email, dataNascmto: dataNascmtoInput.toString() });
-        fs.writeFileSync(arquivo, JSON.stringify(usuarios, null, 4));
-        console.log("Usuário cadastrado com sucesso!");
-    }
-    else if (cliente == "2") {
-        // Login
-        var email_1 = input("Nome: ");
-        var senha_1 = input("Senha: ");
-        var user = usuarios.find(function (u) { return u.email === email_1 && u.senha === senha_1; });
-        if (user) {
-            console.log("Bem-vindo, ".concat(user.nome, "!"));
-        }
-        else {
-            console.log("Usuário ou senha incorretos!");
-        }
-    }
-    else if (cliente === "3") {
-        console.log("Saindo...");
-        return "break";
-    }
-    else {
-        console.log("Opção inválida!");
+    console.log("[1] Cadastro \n[2] Logar \n[3] Sair");
+    var cliente = input("Digite o que deseja fazer: ");
+    switch (cliente) {
+        case "1":
+            // Parte de cadastro
+            var nome = input("Digite seu nome: ");
+            var senha = input("Crie uma senha: ");
+            var cpf_1 = input("Digite seu CPF: ");
+            var email = input("Digite seu email: ");
+            var dataNascmtoInput = input("Digite sua data de nascimento (AAAA-MM-DD): ");
+            var dataNascimento = new Date(dataNascmtoInput);
+            var hoje = new Date();
+            if (dataNascimento > hoje) {
+                console.log("Data de nascimento inválida! Não pode ser no futuro.");
+                break;
+            }
+            // Verifica se o usuário já existe
+            if (usuarios.find(function (u) { return u.cpf == cpf_1; })) {
+                console.log("Usuário já existe!");
+                break;
+            }
+            // Puxa todas as informações e salva em JSON
+            usuarios.push({
+                nome: nome,
+                senha: senha,
+                cpf: cpf_1,
+                email: email,
+                dataNascmto: dataNascimento.toISOString().split("T")[0]
+            });
+            fs.writeFileSync(arquivo, JSON.stringify(usuarios, null, 4));
+            console.log("Usuário cadastrado com sucesso!");
+            break;
+        case "2":
+            // Login
+            var emailLogin_1 = input("Email: ");
+            var senhaLogin_1 = input("Senha: ");
+            var user = usuarios.find(function (u) { return u.email === emailLogin_1 && u.senha === senhaLogin_1; });
+            if (user) {
+                console.log("Bem-vindo, ".concat(user.nome, "!"));
+            }
+            else {
+                console.log("Usuário ou senha incorretos!");
+            }
+            break;
+        case "3":
+            console.log("Saindo...");
+            process.exit(0);
+        default:
+            console.log("Opção inválida!");
+            break;
     }
 };
+// Loop principal do programa (menu: cadastro, login, sair)
 while (true) {
-    var state_1 = _loop_1();
-    if (state_1 === "break")
-        break;
+    _loop_1();
 }
